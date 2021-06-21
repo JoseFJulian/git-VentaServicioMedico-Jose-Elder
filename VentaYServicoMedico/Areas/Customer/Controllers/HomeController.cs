71d543b9dec13279bbdb305d24f8531945b1616d
Elder-AS <57020485+Elder-As@users.noreply.github.com>
2021-06-21 12:45:00 -0600

    Añadido Details de Producto, Roles bien, Arreglo index home

--- VentaYServicoMedico/Areas/Customer/Controllers/HomeController.cs
+++ VentaYServicoMedico/Areas/Customer/Controllers/HomeController.cs
@@ -46,7 +46,100 @@ namespace VentaYServicoMedico.Controllers
             }
             return View(IndexVM);
         }
+        [Authorize]
+        //[Authorize(Roles = "Manager,Counter,Sales")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var MenuItemFromDb =
+              await _db.MenuItem.Include(m => m.Category).Include(
+                  m => m.SubCategory).Where(m => m.Id == id).FirstOrDefaultAsync();
+            //if (MenuItemFromDb.Dietary.Equals("2"))
+            //{
+            //    ViewBag.Dieta = "Dietary";
+            //}
+            //if (MenuItemFromDb.Dietary.Equals("1"))
+            //{
+            //    ViewBag.Dieta = "Not Dietary";
+            //}
+            //if (MenuItemFromDb.Dietary.Equals("3"))
+            //{
+            //    ViewBag.Dieta = "Very Dietary";
+            //}
 
+            string strFecha = MenuItemFromDb.Category.RegisterDate.ToString();
+            //"23/05/2021 0:00:00"
+            //"2021-05-23"
+            strFecha = strFecha.Substring(6, 4) + "-" + strFecha.Substring(3, 2) +
+                       "-" + strFecha.Substring(0, 2);
+            ViewBag.Fecha = strFecha;
+            ShoppingCart carObj = new ShoppingCart()
+            {
+                MenuItem = MenuItemFromDb,
+                MenuItemId = MenuItemFromDb.Id
+            };
+            return View(carObj);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(ShoppingCart CartObject)
+        {
+            CartObject.Id = 0;
+            if (ModelState.IsValid)
+            {
+                //obtener todas las notificaciones de autorizacion del usuario actual
+                //Name, Role, etc (ClaimsType) desde la app en uso
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                //Una vez obtenido el usuario y sus propiedades, buscamos en la base de datos
+                //para verificar la autenticidad y asignar los roles que tiene
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                //la propiedad ApplicationUserID se le asignan los valores de claim
+                CartObject.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = await _db.ShoppingCart.Where(c => c.ApplicationUserId
+                                          == CartObject.ApplicationUserId
+                                          && c.MenuItemId == CartObject.MenuItemId).FirstOrDefaultAsync();
+                if (cartFromDb == null)
+                {
+                    await _db.ShoppingCart.AddAsync(CartObject);
+                }
+                else
+                {
+                    cartFromDb.Count = cartFromDb.Count + CartObject.Count;
+                }
+                await _db.SaveChangesAsync();
+                var count = _db.ShoppingCart.Where(c =>
+                              c.ApplicationUserId == CartObject.ApplicationUserId).ToList().Count();
+                HttpContext.Session.SetInt32(SD.ssShoppingCartCount, count);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var MenuItemFromDb = await _db.MenuItem.Include(m =>
+                                     m.Category).Include(m => m.SubCategory).Where(m =>
+                                     m.Id == CartObject.MenuItemId).FirstOrDefaultAsync();
+                //if (MenuItemFromDb.Dietary.Equals("2"))
+                //{
+                //    ViewBag.Dieta = "Dietary";
+                //}
+                //if (MenuItemFromDb.Dietary.Equals("1"))
+                //{
+                //    ViewBag.Dieta = "Not Dietary";
+                //}
+                //if (MenuItemFromDb.Dietary.Equals("3"))
+                //{
+                //    ViewBag.Dieta = "Very Dietary";
+                //}
+                ShoppingCart cartObj = new ShoppingCart()
+                {
+                    MenuItem = MenuItemFromDb,
+                    MenuItemId = MenuItemFromDb.Id
+                };
+                return View(cartObj);
+            }
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -57,5 +150,6 @@ namespace VentaYServicoMedico.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
